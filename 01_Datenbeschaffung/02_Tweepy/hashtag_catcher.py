ebe86e77b5440cd5b718c1af3abb604ef59f0fc5
ThesisCoacher <arne.gruettner@unisg.ch>
2019-03-07 09:22:15 +0100

    Tweepy und Scrapy Skripte hinzugefügt

---
+++ 01_Datenbeschaffung/02_Tweepy/hashtag_catcher.py
@@ -0,0 +1,28 @@
+# Der folgende Skript basiert auf https://gist.github.com/vickyqian/f70e9ab3910c7c290d9d715491cde44c
+# Welche Moelgichkeiten bietet die Twitter API http://docs.tweepy.org/en/v3.5.0/api.html#timeline-methods
+import tweepy
+import csv
+import pandas as pd
+
+# Authentifizierung mit Twitter. Twitter API credentials. Sind auf der Twitter Developer Seite zu finden, wo ihr Euren Account gemacht habt.
+consumer_key = 'jNoyds3dIvolZmuwvKLl0lRI0'
+consumer_secret = 'VZ7EawnfdNdWNx55h8qieiesjoOLBaQxE1lXs8qmFWVL0aiXJJ'
+access_token = '1069878781110902784-rnoI1fYqfuw81Gh35h4EVT82aIhCp9'
+access_token_secret = 'Jv0GIHE6b0ZKcXh8ymGvHRq6znu0oEmScIqYYy2VkShye'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+# Hashtag hier eingeben
+deinhashtag = "#borussia"
+
+# Oeffnen und schreiben in die CSV Datei
+csvFile = open('%s_hashtag.csv' % deinhashtag, 'a')
+csvWriter = csv.writer(csvFile)
+# Durchsuchen der API nach Hashtag, Sprache und Datum
+for tweet in tweepy.Cursor(api.search,q = deinhashtag,count=1,
+                           lang="de",
+                           since="2018-01-11").items():
+    print (tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
